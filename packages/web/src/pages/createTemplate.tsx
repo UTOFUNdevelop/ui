65174374fea1e68f65b059fa57aa1da940fd9f0d
silgal <52303631+silgal@users.noreply.github.com>
2022-04-28 14:20:21 +0800

    Remove @utofun/ui

--- packages/web/src/pages/createTemplate.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function createTemplate(Component: React.ComponentType) {
-  return function Template(args: {}) {
-    // Isolate eslint-disable-next-line to a single place
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...args} />;
-  };
-}
